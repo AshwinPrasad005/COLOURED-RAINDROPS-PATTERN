68c24b14e5bf235445827aafd07d00ff7b64cec7
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-05-26 12:03:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-var raindrop1;
+var raindrop1engine,world;
+
+const Engine = Matter.Engine;
+const Bodies = Matter.Bodies;
+const World = Matter.World;
+const Constraint = Matter.Constraint;
 
 function setup() {
   createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
   raindrop1 = new Raindrops(50,50,10,70);
 }
 
