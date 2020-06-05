b82ae01276b6e648f2dea8d362f66be2f361e48d
AshwinPrasad005 <64143870+AshwinPrasad005@users.noreply.github.com>
2020-06-05 19:16:19 +0530

    Add files via upload

--- Raindrops.js
+++ Raindrops.js
@@ -5,23 +5,24 @@ class Raindrops{
         density:1.0,
         friction:0.3
        }
+
        this.body = Bodies.rectangle(x,y,width,height,options);
-       this.width = width
-       this.height = height
+    //    this.width = width
+    //    this.height = height
        this.visibility = 255;
        World.add(world,this.body);
     }
     display(){
-        //var angle = this.body.angle;
-        pop();
-        //translate(this.body.position.x,this.body.position.y);
-        //rotate(angle);
-        if(this.body.position.y > 200){
-            this.visibility = this.visibility - 10;
-            tint(255,this.visibility);
-        }
-        rectMode(CENTER)
-        rect(this.body.position.x,this.body.position.y,this.width,this.height);
-        push();
+        // //var angle = this.body.angle;
+        // pop();
+        // //translate(this.body.position.x,this.body.position.y);
+        // //rotate(angle);
+        // if(this.image.position.y > 200){
+        //     this.visibility = this.visibility - 10;
+        //     tint(255,this.visibility);
+        // }
+        // imageMode(CENTER)
+        // image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+        // push();
     }
 }
--- greenRaindrops.js
+++ greenRaindrops.js
@@ -1,9 +1,21 @@
 class GreenRaindrops extends Raindrops{
     constructor(x,y,width,height){
-        super(x,y,width,height);
-        fill("green");
+         super(x,y,width,height);
+        // fill("green");
+        this.image = loadImage("birds_PNG93.png");
     }
     display(){
-        super.display();
+       // super.display();
+                var angle = this.body.angle;
+                pop();
+                translate(this.body.position.x,this.body.position.y);
+                rotate(angle);
+                if(this.body.position.y > 200){
+                    this.visibility = this.visibility - 10;
+                    tint(255,this.visibility);
+                }
+                imageMode(CENTER)
+                image(this.image,this.body.position.x,this.body.position.y,this.width,this.height);
+                push();
     }
 };
